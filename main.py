83e88020bcea35941b518aee545a8d0baca9acbe
[Test] DanielGMedina <danielgmedinatest@gmail.com>
2026-04-28 18:29:38 +0200

    new layout

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import streamlit as st
 import pandas as pd
 
+st.set_page_config(page_title="Uber Rides Data", layout="wide")
+st.title("Uber Rides Data Visualization")
+
+with st.sidebar:
+    st.header("About")
+    st.write(
+        """
+        This app visualizes Uber rides data using Streamlit and Plotly.
+        The data is sourced from a CSV file containing information about Uber rides, including pickup and dropoff locations, timestamps, and more.
+        """
+    )
+
 
 @st.cache_data  # 👈 Add the caching decorator
 def load_data(url):
